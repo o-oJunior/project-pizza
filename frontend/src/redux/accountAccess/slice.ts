50e3c1beeb8f3c3620fdcaca1c5f1036b58c8178
Édio Júnior <ediomendesjunior@hotmail.com>
2024-02-13 21:19:43 -0300

    feat: created slice in redux/accountAccess

---
+++ frontend/src/redux/accountAccess/slice.ts
@@ -0,0 +1,28 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit'
+
+export interface IAccountAccess {
+  modal: boolean
+  hasAccount: boolean
+}
+
+const initialState: IAccountAccess = {
+  modal: false,
+  hasAccount: false,
+}
+
+export const accountAccessSlice = createSlice({
+  name: 'accountAccess',
+  initialState,
+  reducers: {
+    openModal: (state, action: PayloadAction<boolean>) => {
+      state.modal = action.payload
+    },
+    manageAccountAcess: (state, action: PayloadAction<boolean>) => {
+      state.hasAccount = action.payload
+    },
+  },
+})
+
+export const { openModal, manageAccountAcess } = accountAccessSlice.actions
+
+export const useAccountAccess = (state: any) => state.accountAccess as IAccountAccess
