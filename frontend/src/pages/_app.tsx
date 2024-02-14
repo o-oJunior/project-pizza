a979c4c567ed7c81ec8d51f053fb17e0e09ac49b
Édio Júnior <ediomendesjunior@hotmail.com>
2024-02-13 21:20:49 -0300

    feat: added providers in _app.tsx

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,20 +4,23 @@ import './globals.scss'
 import Head from 'next/head'
 
 import { Inter } from 'next/font/google'
+import Providers from '@/redux/providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <title>Pizzaria</title>
-      </Head>
-      <div className={inter.className}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </div>
+      <Providers>
+        <Head>
+          <title>Pizzaria</title>
+        </Head>
+        <div className={inter.className}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </div>
+      </Providers>
     </>
   )
 }
