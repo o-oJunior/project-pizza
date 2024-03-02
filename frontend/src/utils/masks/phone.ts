e029eeb58e79a28ce38543ba4718cf883028cd6b
Édio Júnior <ediomendesjunior@hotmail.com>
2024-03-02 17:30:06 -0300

    feat: created mask phone

---
+++ frontend/src/utils/masks/phone.ts
@@ -0,0 +1,6 @@
+export function maskPHONE(value: string) {
+  value = value.replace(/\D/g, '')
+  value = value.replace(/^(\d{2})(\d)/g, '($1) $2')
+  value = value.replace(/(\d)(\d{4})$/, '$1-$2')
+  return value
+}
