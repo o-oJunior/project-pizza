a8de655abc018e76384b407ce370d2df0df6fb43
Édio Júnior <ediomendesjunior@hotmail.com>
2024-03-03 00:17:02 -0300

    feat: created function to transform password to hash

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,15 +6,7 @@ import 'dotenv/config'
 const port = process.env.PORT
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  app.enableCors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  })
+  const app = await NestFactory.create(AppModule, { cors: true })
   app.useGlobalPipes(new ValidationPipe({ transform: true }))
   await app.listen(port)
 }
