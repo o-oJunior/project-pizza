536ac817c7fa04fbf506c98594dff86564fe6a6c
Édio Júnior <ediomendesjunior@hotmail.com>
2024-04-12 06:49:11 -0300

    refactor: refactored get api

--- frontend/src/api/get/bud.ts
+++ frontend/src/api/get/bud.ts
@@ -1,6 +1,12 @@
-export async function getBud(): Promise<object[]> {
-  const URL_API = process.env.NEXT_PUBLIC_URL_API
-  const response: Response = await fetch(`${URL_API}/bud`)
-  const results = await response.json()
-  return results.data
+import { IResponse } from '@/interfaces/response'
+
+export async function getBud(): Promise<IResponse> {
+  try {
+    const URL_API = process.env.NEXT_PUBLIC_URL_API
+    const response: Response = await fetch(`${URL_API}/bud`)
+    const results = await response.json()
+    return results.data
+  } catch (error) {
+    return { statusCode: 500, error: 'Erro ao buscar os brotos!' }
+  }
 }
--- frontend/src/api/get/pizzaSize.ts
+++ frontend/src/api/get/pizzaSize.ts
@@ -1,6 +1,12 @@
-export async function getPizzaSize(): Promise<object[]> {
-  const URL_API = process.env.NEXT_PUBLIC_URL_API
-  const response: Response = await fetch(`${URL_API}/pizzaSize`)
-  const results = await response.json()
-  return results.data
+import { IResponse } from '@/interfaces/response'
+
+export async function getPizzaSize(): Promise<IResponse> {
+  try {
+    const URL_API = process.env.NEXT_PUBLIC_URL_API
+    const response: Response = await fetch(`${URL_API}/pizzaSize`)
+    const results = await response.json()
+    return results.data
+  } catch (error) {
+    return { statusCode: 500, error: 'Erro ao buscar as pizzas!' }
+  }
 }
